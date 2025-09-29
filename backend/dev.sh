50d54db597cc35b2d01a2577f288bf26f8474f71
mindungil <soet02@jbnu.ac.kr>
2025-09-30 01:50:05 +0900

    feat: 질문 증가 로직 분리

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,3 @@
-export CORS_ALLOW_ORIGIN="http://localhost:5173;http://localhost:8080;http://220.124.155.35:5173;http://220.124.155.35/8083"
+export CORS_ALLOW_ORIGIN="http://localhost:5173;http://localhost:8080;http://220.124.155.35:5173;http://220.124.155.35/8081"
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
